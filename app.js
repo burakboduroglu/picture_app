46f57f0f29d83d87f7c929de647261d846413bf2
bboduroglu <burakboduroglu0@gmail.com>
2023-06-01 22:12:24 +0300

    app.js created

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('Hello World!');
-};
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo Description',
+  };
+  res.status(200).send(photo);
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER PORT: ${PORT}`);
+});
