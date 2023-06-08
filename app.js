075112928d377a2b2be7c0d31450627e99ade169
bboduroglu <burakboduroglu0@gmail.com>
2023-06-08 15:31:29 +0300

    edit page added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Photo = require('./models/Photo');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const methodOverride = require('method-override');
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +21,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
   const photos = await Photo.find({}).sort('-dateCreated');
@@ -60,6 +62,22 @@ app.post('/photos', async (req, res) => {
     });
     res.redirect('/');
   });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 app.listen(PORT, () => {
